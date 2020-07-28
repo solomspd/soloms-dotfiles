1ed87d0cf45db05bd8653dad18ef2b7222f91fde
Abdelsalam ElTamawy <solomspd@aucegypt.edu>
2020-07-28 16:07:16 +0200

    updated lock script to handle notificatoins correctly

--- .scripts/i3lock.sh
+++ .scripts/i3lock.sh
@@ -7,4 +7,8 @@ convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 # convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 # dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 
+dunstify "DUNST_COMMAND_PAUSE"
+
 i3lock -u -i "$tmpbg"
+
+dunstify "DUNST_COMMAND_RESUME"
