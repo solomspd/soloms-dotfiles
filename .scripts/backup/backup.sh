d9fb9e38f2cf88e81a4f3d5ba8da50aeae7cbdd6
Abdelsalam ElTamawy <solomspd@aucegypt.edu>
2020-07-28 18:49:41 +0200

    symlink ifixes

--- .scripts/backup/backup.sh
+++ .scripts/backup/backup.sh
@@ -1,1 +1,6 @@
-/usr/local/solom/backup.sh
+#!/bin/sh
+rsync -av --delete /data/Solom/Solom/Anime /data/Ancalagon/Ancalagon/Anime /remote/Abdo-NAS/main &&
+rsync -av --delete /data/Abdo/Abdo/Abdos\ University\ work /remote/Abdo-NAS/main && 
+rsync -av --delete /data/Abdo/Abdo/images /remote/Abdo-NAS/main &&
+rsync -av --delete /data/Abdo/Abdo/music /remote/Abdo-NAS/main &&
+rsync -av --delete /data/Abdo/Abdo/TV\ shows /data/Solom/Solom/TV\ shows /data/Ancalagon/Ancalagon/TV\ shows /remote/Abdo-NAS/main
