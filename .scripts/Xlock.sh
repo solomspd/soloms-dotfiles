38dd733ab5891bc93522c8b4f3ebc6aceb5fd950
Abdelsalam ElTamawy <solomspd@aucegypt.edu>
2021-05-01 05:18:48 +0200

    added comments cuz why not

--- .scripts/Xlock.sh
+++ .scripts/Xlock.sh
@@ -8,13 +8,14 @@ icon='/home/solom/.scripts/lock.png'
 maim "${tmpbg}" # take screenshot
 convert "${tmpbg}" -scale 10% -scale 1000% "${tmpbg}" # pixilate screenshot
 
-old_IFS="${IFS}"
+old_IFS="${IFS}" # to restore IFS when we're dont manipulating it for good measure
 
-screen=$(xrandr | grep -Eow '([0-9]*x[0-9]*\+[0-9]*\+[0-9]*)' | xargs)
-read -ra screen <<< "${screen}"
-icon_dim=$(convert ".scripts/lock.png" -print "%wx%h\n" /dev/null)
-IFS='x' read -ra icon_dim <<< "${icon_dim}"
+screen=$(xrandr | grep -Eow '([0-9]*x[0-9]*\+[0-9]*\+[0-9]*)' | xargs) # get info on X11 monitors on screen
+read -ra screen <<< "${screen}" # turn list of monitors into array
+icon_dim=$(convert ".scripts/lock.png" -print "%wx%h\n" /dev/null) # get dimentions of lock icon
+IFS='x' read -ra icon_dim <<< "${icon_dim}" # turn dimentions into array
 
+# Loop over each monitor get coordinated to center lock icon and composite it onto screenshot
 for i in "${screen[@]}"; do # i[1] = w px; i[2] = h px; i[3] = x offset; i[4] = y offset
 	IFS='x|+' read -ra i <<< "${i}"
 	newx=$((i[2] + i[0]/2 - icon_dim[0]/2))
@@ -22,7 +23,7 @@ for i in "${screen[@]}"; do # i[1] = w px; i[2] = h px; i[3] = x offset; i[4] = 
 	convert "${tmpbg}" "${icon}" -geometry "+${newx}+${newy}" -composite -matte "${tmpbg}"
 done
 
-IFS="${old_IFS}"
+IFS="${old_IFS}" # restore IFS
 
 # pause music
 playerctl -a pause
@@ -30,6 +31,7 @@ playerctl -a pause
 # pause notifications
 dunstify "DUNST_COMMAND_PAUSE"
 
+# lock screen with generated screenshot
 i3lock -n -u -i "${tmpbg}"
 
 # resume notifications
--- .scripts/Xsuspend.sh
+++ .scripts/Xsuspend.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-~/.scripts/Xlock.sh & (sleep 5s && systemctl suspend)
+~/.scripts/Xlock.sh & (sleep 5s && systemctl suspend) # sleep a bit to give locking script enough time to finish
 
-nvidia-settings -l
+nvidia-settings -l # X11 sometimes doesn't like waking up from suspend so force a display settings refresh
